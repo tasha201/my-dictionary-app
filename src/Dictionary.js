0572bec8be52664f56335696c7ee5fa62fd51a38
tasha201 <natalia.it.designer@gmail.com>
2023-10-31 19:47:56 +0200

    Adding first section of Dictionary.js

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,9 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
+import "./Dictionary.css"
 
-export default function Dictionary() {
+export default function Dictionary(props) {
+
+const [keyword, setKeyword] = useState(props.defaultKeyword);
+
+  function handleKeywordChange(event) {
+    setKeyword(event.target.value);
+}
+
   return (
-    <div>
-
+    <div className="Dictionary">
+      <section>
+        <form>
+          <label>What word do you want to look up?</label>
+          <input
+            type="search"
+            placeholder="Search for a word"
+            defaultValue={props.defaultKeyword}
+            autoFocus={true}
+            className="search-input"
+            onChange={handleKeywordChange}
+          />
+        </form>
+        <small className="hint">i.e. ukraine, love, food, flower</small>
+      </section>
     </div>
   )
 }
