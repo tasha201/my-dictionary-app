8c71316082900839a8272dc7d160d912f97f27ca
tasha201 <natalia.it.designer@gmail.com>
2023-10-31 22:42:26 +0200

    Adding API to Dictionary.js

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,30 +1,60 @@
 import React, { useState } from "react";
-import "./Dictionary.css"
+import axios from "axios";
+import Result from "./Result";
+import Photos from "./Photos.js";
+import "./Dictionary.css";
 
 export default function Dictionary(props) {
+  const [keyword, setKeyword] = useState(props.defaultKeyword);
+  const [definition, setDefinition] = useState(null);
+  const [photos, setPhotos] = useState([]);
 
-const [keyword, setKeyword] = useState(props.defaultKeyword);
+  function handleImages(response) {
+    setPhotos(response.data.photos);
+  }
+
+  function handleResponse(response) {
+    setDefinition(response.data);
+    let apiKey = "eac360db5fc86ft86450f3693e73o43f";
+    let apiUrl = `https://api.shecodes.io/images/v1/search?query=${response.data.word}&key=${apiKey}`;
+    axios
+      .get(apiUrl, { headers: { Authorization: `Bearer ${apiKey}` } })
+      .then(handleImages);
+  }
+
+  function search() {
+    let apiKey = "eac360db5fc86ft86450f3693e73o43f";
+    let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key=${apiKey}`;
+    axios.get(apiUrl).then(handleResponse);
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    search();
+  }
 
   function handleKeywordChange(event) {
     setKeyword(event.target.value);
-}
+  }
 
   return (
     <div className="Dictionary">
       <section>
-        <form>
+        <form onSubmit={handleSubmit}>
           <label>What word do you want to look up?</label>
           <input
             type="search"
             placeholder="Search for a word"
             defaultValue={props.defaultKeyword}
             autoFocus={true}
-            className="search-input"
+            className="form-control search-input"
             onChange={handleKeywordChange}
           />
         </form>
         <small className="hint">i.e. ukraine, love, food, flower</small>
       </section>
+      <Result definition={definition} />
+      <Photos photos={photos} />
     </div>
-  )
+  );
 }
---
+++ src/Photos.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Photos(props) {
+  return (
+    <div>
+
+    </div>
+  )
+}
